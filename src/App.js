5cab546fd034e37db3b2906c325f2665f80a8074
Eko Edita <ekoeditaa@gmail.com>
2019-07-31 22:43:40 +0800

    Initial commit
    Signed-off-by: Eko Edita <ekoeditaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Menu from './components/Menu';
+import Content from './components/Content';
+
 import './App.css';
 
 function App() {
+  const [activePage, setActivePage] = useState('index');
+
+  function handleMenuChange(ev) {
+    setActivePage(ev.currentTarget.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu handleMenuChange={handleMenuChange} />
+      <Content activePage={activePage} />
     </div>
   );
 }
